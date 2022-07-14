6b1b93bac3ee23209cf5ea936621a914f794f37c
ramesh <rameshkumarjayam@gmail.com>
2022-07-14 19:53:43 +0530

    LLLLL

--- training 2/javasc/operators/test19.js
+++ training 2/javasc/operators/test19.js
@@ -1,12 +1,5 @@
-let A=10
-let B=5
-let C=30
-if(A<=B && B<=C){
-        console.log("Greatest number = B");
-    }
-    else if (B<=A && A<=C) {
-        console.log("Greatest number = A");
-    }
-    else if (B<=C && C<=A) {
-        console.log("Greatest number = C");
-    }
+let A =10;
+let B =3;
+let C =20;
+Math.max()
+console.log(Math.max(A,B,C))
